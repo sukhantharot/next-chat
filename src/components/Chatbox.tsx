143fb887efb41af4768990faa2573ff91734b1a3
oatfazwaz <93711042+oatfazwaz@users.noreply.github.com>
2024-01-19 16:38:10 +0700

    add chatbox and SendMessageButton.tsx components

---
+++ src/components/Chatbox.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+  DocumentData,
+} from 'firebase/firestore';
+import { db } from '../firebase';
+import Message from './Message';
+import SendMessage from './SendMessage';
+
+interface MessageItem {
+  id: string;
+  createdAt: number;
+  text: string;
+  name: string;
+  avatar: string;
+  uid: string;
+}
+
+const ChatBox: React.FC = () => {
+  const [messages, setMessages] = useState<MessageItem[]>([]);
+  const scroll = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'messages'),
+      orderBy('createdAt', 'desc'),
+      limit(50)
+    );
+
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      const fetchedMessages: MessageItem[] = [];
+      QuerySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...(doc.data() as MessageItem), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} />
+    </main>
+  );
+};
+
+export default ChatBox;
---
+++ src/components/SendMessageButton.tsx
@@ -0,0 +1,54 @@
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { auth, db } from '@/libs/firebase.ts';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+
+interface SendMessageProps {
+  scroll: React.RefObject<HTMLDivElement>;
+}
+
+const SendMessage: React.FC<SendMessageProps> = ({ scroll }) => {
+  const [message, setMessage] = useState<string>('');
+
+  const sendMessage = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (message.trim() === '') {
+      alert('Enter valid message');
+      return;
+    }
+
+    const { uid, displayName, photoURL } = auth.currentUser!;
+
+    await addDoc(collection(db, 'messages'), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+
+    setMessage('');
+    scroll.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <form onSubmit={(event) => sendMessage(event)} className="send-message">
+      <label htmlFor="messageInput" hidden>
+        Enter Message
+      </label>
+      <input
+        id="messageInput"
+        name="messageInput"
+        type="text"
+        className="form-input__input"
+        placeholder="type message..."
+        value={message}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default SendMessage;
+
